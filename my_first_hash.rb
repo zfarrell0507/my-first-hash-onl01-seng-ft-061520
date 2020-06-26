9cd40fe97d386294c73aa02a06d2c50116e9f80e
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-26 23:39:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  your_hash = {money => 2000, popular => clout, science => power}
+  your_hash = {year => 2000, month => 5, day => 7}
   your_hash = my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   
