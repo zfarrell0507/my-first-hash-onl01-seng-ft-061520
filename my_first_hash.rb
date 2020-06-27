2ff489a1e3c82987b28ac48e7bff53fba101d772
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 15:01:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,8 +15,7 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest
-  retrieval = shipping_manifest
+  shipping_manifest["oil paintings"]
 end
 retrieval
 
