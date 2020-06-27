433fd45a82b3d76affce6e041aeab38da34a4610
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 14:53:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -48,34 +48,3 @@ shipping_manifest
   # return the shipping_manifest hash below
 
 end
-
-context "Challenge II: Hash with Data" do 
-  describe "#shipping_manifest" do 
-    it "returns a hash with key/value pairs describing old-timey items" do 
-      expect(shipping_manifest.keys).to match_array(["whale bone corsets", "oil paintings", "porcelain vases"])
-      expect(shipping_manifest.values).to match_array([2, 5, 3])
-    end
-  end
-end
-
-context "Challenge III: Retrieving Data" do 
-  describe "#retrieval" do 
-    it "operates on the shipping_manifest hash to return the value of the 'oil paintings' key" do 
-      expect(retrieval).to eq(3)
-    end
-  end
-end
-
-context "Challenge IV: Adding Data" do 
-  describe "#adding" do 
-    it "operates on the shipping_manifest hash to add a key/value pair" do 
-      expect(adding).to eq({
-                      "whale bone corsets" => 5, 
-                      "porcelain vases" => 2, 
-                      "oil paintings" => 3, 
-                      "muskets" => 2,
-                      "gun powder" => 4
-                      })
-    end
-  end
-end
