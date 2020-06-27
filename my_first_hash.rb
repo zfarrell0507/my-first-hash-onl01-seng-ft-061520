a2d5efb3a321e23cbf304030be58be4ef1440d89
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 14:03:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,10 +19,10 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
+retrieval
+
 
 def adding
   shipping_manifest = {
