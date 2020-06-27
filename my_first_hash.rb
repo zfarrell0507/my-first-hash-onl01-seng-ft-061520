dd60948bfd4b5a348f39d797d6e52cecdfba7150
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 14:06:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,7 +19,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-shipping_manifest["oil paintings"]
+puts shipping_manifest["oil paintings"]
 end
 retrieval
 
