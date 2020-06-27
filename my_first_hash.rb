33aba8f7dc899eda4e80e5d8d1bdbba7f5b12580
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 14:30:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,14 +32,12 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2 
-  shipping_manifest["muskets"]
 
 
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gun powder"] = 4
-shipping_manifest["gun powder"]
 puts shipping_manifest
-
+shipping_manifest
 
   # return the shipping_manifest hash below
 
