1ece4e2e5312f89ef69f499fb88b9da8af415380
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 14:25:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,7 +31,9 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"]
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["muskets"]
+end
 
 
   # add 4 gun powder to the shipping_manifest hash below
